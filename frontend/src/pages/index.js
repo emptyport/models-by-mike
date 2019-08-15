2d1b859c5d3f3fff7a8ffd4be7ae6cc1ceb5a1e6
Michael Porter <michael.porter@utsouthwestern.edu>
2019-08-15 08:24:53 -0500

    adding in OLC badge

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -81,6 +81,7 @@ class IndexPage extends React.Component{
 								<text fill="url(#wallPattern)" textAnchor="middle" x="50" y="50">{this.props.data.site.siteMetadata.capitalizeTitleOnHome ? this.props.data.site.siteMetadata.title.toUpperCase() : this.props.data.site.siteMetadata.title}</text>
 							</svg>
 						</div>
+						<img width="200" src="/images/OLC_badge.png"></img>
 						<p className="tag-line text-secondary">{this.props.data.site.siteMetadata.introTag}</p>
 						<p className="caption text-tertiary">{this.props.data.site.siteMetadata.description}</p>
 						<a href="#portfolio" className="btn">SEE WORKS</a>
